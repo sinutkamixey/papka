50e829884d0e6bc7679a6cfd3a31e4d483f1945e
sinutkamihey <toma.020396@gmail.com>
2017-09-19 16:03:32 +0700

    Задание

--- index.php
+++ index.php
@@ -4,3 +4,16 @@ $currentyear=2017;
 $m=$currentyear-$myyear;
 echo $m; 
 ?>
+<?php
+$myyear=1996;
+$mymonth=3;
+$myday=2;
+$currentyear=2017;
+$currentmonth=9;
+$currentday=19;
+if($mymonth<$currentmonth){
+	echo "День рождения уже прошел"
+} else {
+	echo "День рождения еще не наступил"
+}
+?>
