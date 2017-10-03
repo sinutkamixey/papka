4098e3f81f376fe3ebe22b1cd4fb29c03b37d8b2
sinutkamihey <toma.020396@gmail.com>
2017-10-03 14:55:52 +0700

    03/10/2017

--- zadacha.php
+++ zadacha.php
@@ -1,11 +1,13 @@
 <?php
 $prices=[120, 130, 150, 130, 100];
 $prices[0]=50;
-$prices[]=100;
-summ=0;
-for ($i=0; $i<count($prices): $i++){
-$summ+=$prices[$i];
+$prices[100]=200;
+$summ=0;
+for ($i=0; $i<count($prices); $i++){
+	if(isset($prices[$i])) {
+		$summ+=$prices[$i];
+	}
 }
-$avg=$prices/count($prices);
+$avg=$summ/count($prices);
 echo "Среднее арифметическое $avg"; 
 ?>
